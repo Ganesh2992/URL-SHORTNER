17f7f46b46539665ae7b101052c57f8084397a06
Ganesh2992 <ganeshmangalamsinha@gmail.com>
2026-02-02 02:53:48 +0530

    Modified Routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import { signupPostRequestBodySchema } from '../validation/request.validation.js';
+import { signupPostRequestBodySchema,loginPostRequestBodySchema} from '../validation/request.validation.js';
 import {hashedPasswordWithSalt} from '../utils/hash.js';
 import {getUserByEmail,createUser} from '../services/user.service.js';
+import {createUserToken} from '../utils/token.js';
 
 const router = express.Router();
 
@@ -31,5 +32,30 @@ router.post('/signup',async(req,res)=>{
   });
 
   return res.status(201).json({data:{userId: user.id}});
+});
+
+router.post('/login',async(req,res)=>{
+  const validationResult = await loginPostRequestBodySchema.safeParseAsync(req.body);
+
+  if(validationResult.error){
+    return res.status(400).json({error: validationResult});
+  }
+
+  const {email,password} =validationResult.data;
+
+  const user = await getUserByEmail(email);
+
+  if(!user){
+    return res.status(404).json({error : `User with eamil ${email} does not exist`});
+  }
+
+  const {password: hashedPassword} = hashedPasswordWithSalt(password,user.salt);
+
+  if(user.password!==hashedPassword){
+    res.status(400).json({error: 'Invalid Password'});
+  }
+  const token = await createUserToken({id:user.id});
+
+  return res.json({token});
 })
 export default router;
