c183aad89de175a827795a7394fabf05f914a05b
Ganesh2992 <ganeshmangalamsinha@gmail.com>
2026-02-02 02:59:48 +0530

    Ave

--- utils/hash.js
+++ utils/hash.js
@@ -1,10 +1,8 @@
 import {randomBytes,createHmac, hash} from 'crypto';
 
-
 export function hashedPasswordWithSalt(password,userSalt=undefined){
   const salt = userSalt ?? randomBytes(256).toString('hex');
   const hashedPassword = createHmac('sha256',salt).update(password).digest('hex');
 
   return {salt,password:hashedPassword};
 }
-
